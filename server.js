43f7e05649a04c76b16cbd1356f524ee4b7d7985
narinsss <maarib03@gmail.com>
2022-03-24 01:32:07 -0300

    editserver2

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static('./dist/breakfast'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/breakfast/'));
 
-app.get('/*', function(req, res) {
-res.sendFile('index.html', {root: 'dist/breakfast/'}
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/breakfast/'}),
 );
-});
 
-app.listen(process.env.PORT || 3000, function(){
-console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
